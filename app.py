a26050e9c5d380e2bf1367feb232aee717db6792
Milton Storm <m.storm@itecriocuarto.org.ar>
2023-06-22 23:11:48 -0300

    Add route index in app.py

--- app.py
+++ app.py
@@ -2,3 +2,10 @@ import requests
 from flask import Flask, render_template, request
 
 app = Flask(__name__, static_folder='static')
+
+@app.route('/')
+def index():
+    return render_template(
+        'index.html'
+    )
+
